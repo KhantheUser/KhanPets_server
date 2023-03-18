755d60a7b3b91bc7caa1d94d01ae19a89468ef8d
Thien Duc <ducthiennguyen1601@gmail.com>
2023-03-18 20:23:51 +0700

    18-3-7

--- app.js
+++ app.js
@@ -24,12 +24,23 @@ dotenv.config({
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: "https://khan-pets-server.vercel.app/api/checkout",
-//   })
-// );
+
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://khan-pets-server.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
