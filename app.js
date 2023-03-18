b941a18c70c6ce3bb2b1297ab0f4b64f0566c9a5
Thien Duc <ducthiennguyen1601@gmail.com>
2023-03-18 20:05:31 +0700

    18-3-4

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://khan-pets-server.vercel.app",
+    origin: "https://khan-pets-server.vercel.app/api/checkout",
   })
 );
 app.set("view engine", "html");
